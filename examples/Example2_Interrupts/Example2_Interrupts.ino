7a56ff751800f26e033d174d6e1b808d29a35364
MadisonC-SparkFun <madison.chodikov@sparkfun.com>
2023-10-23 12:57:22 -0600

    Added comment header section to examples
    Added more documentation and links to the examples.

--- examples/Example2_Interrupts/Example2_Interrupts.ino
+++ examples/Example2_Interrupts/Example2_Interrupts.ino
@@ -1,3 +1,50 @@
+/******************************************************************************
+  Example2_Interrupts.ino
+  
+  Read human presence detection value from the STHS34PF80 sensor, print them
+  to terminal using the interrupt flag instead of the typical data ready flag. 
+  Prints raw IR presence values (cm^-1).
+
+  SparkFun STHS34PF80 Arduino Library
+  Madison Chodikov @ SparkFun Electronics
+  Original Creation Date: September 19th, 2023
+  https://github.com/sparkfun/SparkFun_STHS34PF80_Arduino_Library
+
+  Development environment specifics:
+
+  IDE: Arduino 2.2.1
+  Hardware Platform: SparkFun RedBoard Qwiic	
+  SparkFun Human Presence and Motion Sensor - STHS34PF80 (Qwiic) Version: 1.0
+  SparkFun Qwiic Mini Human Presence and Motion Sensor - STHS34PF80 Version: 1.0
+
+  Do you like this library? Help support SparkFun. Buy a board!
+
+    SparkFun Human Presence and Motion Sensor - STHS34PF80 (Qwiic)
+    https://www.sparkfun.com/products/22494
+    
+    SparkFun Qwiic Mini Human Presence and Motion Sensor - STHS34PF80
+    https://www.sparkfun.com/products/23253
+
+  Hardware Connections:
+  Use a Qwiic cable to connect from the RedBoard Qwiic to the STHS34PF80 breakout (QWIIC).
+  You can also choose to wire up the connections using the header pins like so:
+
+  ARDUINO --> STHS34PF80
+  SDA (A4) --> SDA
+  SCL (A5) --> SCL
+  INT (A3) --> INT
+  3.3V --> 3.3V
+  GND --> GND
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #include "SparkFun_STHS34PF80_Arduino_Library.h"
 #include <Wire.h>
 
--- examples/Example4_SPIFunctionality/Example4_SPIFunctionality.ino
+++ examples/Example4_SPIFunctionality/Example4_SPIFunctionality.ino
@@ -1,3 +1,51 @@
+/******************************************************************************
+  Example4_SPIFunctionality.ino
+  
+  Read human presence detection values from the STHS34PF80 sensor, print them
+  to terminal using SPI communication instead of I2C. 
+  Prints raw IR presence (cm^-1), if motion was detected, and temperature 
+  in degrees C.
+
+  SparkFun STHS34PF80 Arduino Library
+  Madison Chodikov @ SparkFun Electronics
+  Original Creation Date: September 19th, 2023
+  https://github.com/sparkfun/SparkFun_STHS34PF80_Arduino_Library
+
+  Development environment specifics:
+
+  IDE: Arduino 2.2.1
+  Hardware Platform: SparkFun RedBoard Qwiic	
+  SparkFun Human Presence and Motion Sensor - STHS34PF80 (Qwiic) Version: 1.0
+  SparkFun Qwiic Mini Human Presence and Motion Sensor - STHS34PF80 Version: 1.0
+
+  Do you like this library? Help support SparkFun. Buy a board!
+
+    SparkFun Human Presence and Motion Sensor - STHS34PF80 (Qwiic)
+    https://www.sparkfun.com/products/22494
+    
+    SparkFun Qwiic Mini Human Presence and Motion Sensor - STHS34PF80
+    https://www.sparkfun.com/products/23253
+
+  Hardware Connections:
+  Wire the SPI Connections from the RedBoard Qwiic to the STHS34PF80 Breakout 
+  with a resistive divider using the header pins like so: 
+
+  ARDUINO --> STHS34PF80
+  SCK/SCL (8) --> Clock
+  SDI/SDA (12) --> Data in
+  !CS (13) --> Chip Select
+  3.3V --> 3.3V
+  GND --> GND
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #include "SparkFun_STHS34PF80_Arduino_Library.h"
 #include <SPI.h>
 
